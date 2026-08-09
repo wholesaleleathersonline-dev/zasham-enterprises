c4c31c8cb92633e0a5434cbf400418e8ba378f8d
Hamid Shahbaz <wholesaleleathersonline@gmail.com>
2026-08-09 18:57:14 +0500

    Fix dynamic sports categories

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,8 @@ import Reveal from "../components/animation/Reveal";
 import { getHeroBanner } from "../services/admin/heroBanner.service";
 
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const heroBanner = await getHeroBanner();
   return (
